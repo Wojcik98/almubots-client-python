faeb7a5fd635ec19d186341bb1dd9b2690b1addd
Tomasz <kurek.tom@gmail.com>
2020-04-23 14:40:20 +0200

    Make ShootLowestLifeBot runner compatible

--- shoot_lowest_life.py
+++ shoot_lowest_life.py
@@ -2,50 +2,52 @@ from math import atan2, pi
 
 from almubots_comm import Comm
 
-botId = 1
 
-if __name__ == '__main__':
-    comm = Comm(botId)
+class ShootLowestLifeBot:
+    def __init__(self, bot_num):
+        self.bot_num = bot_num
+        self.comm = Comm(bot_num)
 
-    while True:
-        status = comm.send()
-        me = status['bots'][botId]
-        enemy = status['bots'][0]
+    def run(self):
+        while True:
+            status = self.comm.send()
+            me = status['bots'][self.bot_num]
+            enemy = status['bots'][0]
 
-        for bot in status['bots']:
-            if bot is not me and bot['life'] != 0:
-                if enemy['life'] > bot['life']:
-                    enemy = bot
+            for bot in status['bots']:
+                if bot is not me and bot['life'] != 0:
+                    if enemy['life'] > bot['life']:
+                        enemy = bot
 
-        alfa = atan2(enemy['y'] - me['y'], enemy['x'] - me['x'])
-        alfa = alfa * 180 / pi
-        angle_diff = abs(me['angle']-alfa)
-        if alfa > me['angle']:
-            if angle_diff <= 180:
-                comm.rotate(1)
-            else:
-                comm.rotate(-1)
-        if alfa < me['angle']:
-            if angle_diff <= 180:
-                comm.rotate(-1)
-            else:
-                comm.rotate(1)
+            angle = atan2(enemy['y'] - me['y'], enemy['x'] - me['x'])
+            angle = angle * 180 / pi
+            angle_diff = abs(me['angle']-angle)
+            if angle > me['angle']:
+                if angle_diff <= 180:
+                    self.comm.rotate(1)
+                else:
+                    self.comm.rotate(-1)
+            if angle < me['angle']:
+                if angle_diff <= 180:
+                    self.comm.rotate(-1)
+                else:
+                    self.comm.rotate(1)
 
-        dx = 0
-        dy = 0
-        if enemy['x'] > me['x']:
-            dx = 1
-        if enemy['y'] > me['y']:
-            dy = 1
-        if enemy['x'] < me['x']:
-            dx = -1
-        if enemy['y'] < me['y']:
-            dy = -1
+            dx = 0
+            dy = 0
+            if enemy['x'] > me['x']:
+                dx = 1
+            if enemy['y'] > me['y']:
+                dy = 1
+            if enemy['x'] < me['x']:
+                dx = -1
+            if enemy['y'] < me['y']:
+                dy = -1
 
-        comm.move(dx, dy)
+            self.comm.move(dx, dy)
 
-        shoot = False
-        if abs(me['angle'] - alfa) < 10:
-            shoot = True
+            shoot = False
+            if abs(me['angle'] - angle) < 10:
+                shoot = True
 
-        comm.shoot(shoot)
+            self.comm.shoot(shoot)
